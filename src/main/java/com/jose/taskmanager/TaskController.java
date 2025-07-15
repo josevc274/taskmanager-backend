49751db34bcb71bad68227dccd866f4055619d30
Jose Corvo <josevc27us@gmail.com>
2025-07-15 12:09:11 -0400

    Initial commit

---
+++ src/main/java/com/jose/taskmanager/TaskController.java
@@ -0,0 +1,29 @@
+package com.jose.taskmanager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tasks")
+public class TaskController {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteTask(@PathVariable Long id) {
+        taskRepository.deleteById(id);
+    }
+}
